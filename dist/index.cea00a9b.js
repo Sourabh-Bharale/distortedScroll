c3ea65971f6e1d28ba68eb24868a5a586e00373c
Sourabh-Bharale <89797467+Sourabh-Bharale@users.noreply.github.com>
2022-07-13 19:03:38 +0530

    scroll animation✅

--- dist/index.cea00a9b.js
+++ dist/index.cea00a9b.js
@@ -1,2 +1,20 @@
+const scrollable = document.querySelector(".scrollable");
+let current = 0;
+let target = 0;
+let ease = 0.001;
+function lerp(start, end, t) {
+    return start * (1 - t) + end * t;
+}
+function init() {
+    document.body.style.height = `${scrollable.getBoundingClientRect().height}px`;
+}
+function smoothScroll() {
+    target = window.scrollY;
+    current = lerp(current, target, ease);
+    scrollable.style.transform = `translate3d(0,${-current}px,0)`;
+    requestAnimationFrame(smoothScroll);
+}
+init();
+smoothScroll();
 
 //# sourceMappingURL=index.cea00a9b.js.map
